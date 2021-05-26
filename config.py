6b4e2cb0a2eb1d2e0a0289723dd76ab5d936324c
HajimeK <smec.kawata@gmail.com>
2021-05-26 18:22:35 +0900

    Coded locally (Auth error, tries to read another account which is used in the same PC even specified)

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = "59d5f340-06de-42ba-bb52-427fbc55ca3d"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -27,10 +27,11 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
+    # AUTHORITY = "https://login.microsoftonline.com/60e63508-9cfd-4013-9a1f-7610707b07ce"  # For multi-tenant app, else put tenant name
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "6891b149-5ed4-4e1c-82b3-f37c3b30b109"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
